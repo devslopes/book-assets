e3def23623157d8889be96298bc1fb7c2a4f43ce
Caleb Stultz <caleb@devslopes.com>
2017-01-26 18:05:22 +0700

    Updated UIStackView source code.

---
+++ section-2-chapter-2-uistackview/UIStackViews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIStackViews
+//
+//  Created by Evan Leong on 1/24/17.
+//  Copyright © 2017 Evan Leong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
